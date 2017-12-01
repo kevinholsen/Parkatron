2c481e5222c1201ed29e2722edbd9ec900aaf1b9
Kevin Olsen <kevinolsen@Kevins-MacBook-Pro-2.local>
2017-12-01 09:16:48 -0500

    First commit

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -19,9 +19,9 @@ export class ListPage {
     'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i < 6; i++) {
       this.items.push({
-        title: 'Item ' + i,
+        title: 'Lot' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
